981c94d9a96ac94acccd0b7e6aacd0075a99b22a
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-31 20:27:49 +0800

    Updated Punch.lua

--- Modules/Punch.lua
+++ Modules/Punch.lua
@@ -1,29 +1,66 @@
 local Punch = {}
+local players = game:GetService("Players")
 local Hitbox = require(script.Parent["Magnitude Hitbox"])
 local Block = require(script.Parent.Block)
+local animationPlayer = require(script.Parent.Parent.Miscellaneous.Animation)
+local Knockback = require(script.Parent.Knockback)
+local Damage = require(script.Parent.Damage)
+local Stun = require(script.Parent.Stun)
 local damageDebounce = {}
 
-function Punch.Punch(player)
-	damageDebounce[player.UserId] = false
-	local char = player.Character
-	if not char then return end
+function Punch.Punch(character, isPlayer, velocity)
+	if not character then return end
+	
+	local hrp = character:WaitForChild("HumanoidRootPart")
+	local humanoid = character:FindFirstChildWhichIsA("Humanoid")
+	if not hrp or not humanoid then return end
+	
+	local playerUserId
+	local botID = not isPlayer and humanoid.Parent:GetAttribute("BotID") or nil 
 
-	local hrp = char:WaitForChild("HumanoidRootPart")
+	if isPlayer then
+		local player = players:GetPlayerFromCharacter(character)
+		playerUserId = player.UserId
+		damageDebounce[playerUserId] = damageDebounce[playerUserId] or false
+	elseif botID then
+		damageDebounce[botID] = damageDebounce[botID] or false
+	else
+		return
+	end
+
 	local hit = Hitbox.Complex(hrp, 7)
 	if not hit or not hit:IsA("Model") then return end
 
-	local humanoid = hit:FindFirstChildWhichIsA("Humanoid")
-	if not humanoid then return end
+	local targetHumanoid = hit:FindFirstChildWhichIsA("Humanoid")
+	if not targetHumanoid then return end
 
 	local blocked = Block.BlockCheck(hrp.Position, hit)
-	if not blocked and not damageDebounce[player.UserId] then
-		damageDebounce[player.UserId] = true
-		humanoid:TakeDamage(10)
-		task.delay(0.1, function()
-			damageDebounce[player.UserId] = false
-		end)
+	if not blocked  then
+		Damage.ApplyDamageFromMagnitudeHitbox(character, hit, 10, true)
 	end
 end
 
+function Punch.Complex(char, animations, punchCount, punchLimit, velocity, isPlayer)
+	if punchCount < punchLimit then
+		local track = animationPlayer.PlayAnimationThatHasAnAnimationEvent(char, animations["Punch" .. punchCount])
+		task.spawn(function()
+			Stun.StunWithAnimation(char, 6, track)
+		end)	
+		track:GetMarkerReachedSignal("Punch"):Connect(function()
+			Punch.Punch(char, isPlayer, velocity)
+		end)
+		return punchCount + 1
+	elseif punchCount >= punchLimit then
+		local track = animationPlayer.PlayAnimationThatHasAnAnimationEvent(char, animations["Punch" .. punchCount])
+		task.spawn(function()
+			Stun.StunWithAnimation(char, 6, track)
+		end)	
+		track:GetMarkerReachedSignal("Punch"):Connect(function()
+			Punch.Punch(char, isPlayer, velocity)
+		end)
+		punchCount = 1
+		return punchCount 
+	end	
+end
 
 return Punch
