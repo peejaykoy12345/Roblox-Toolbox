d7c1234727ab14ba8bb34f81abca747242cbab4c
peejaykoy12345 <rick.well.rick@gmail.com>
2025-08-26 07:58:25 +0800

    Added more functions to AnimationPlayer
    Added PlayAndHoldAnimation and StopAndCleanup

--- Modules/Animation Player.lua
+++ Modules/Animation Player.lua
@@ -1,73 +1,3 @@
---[[local Animation = {}
-
-local cachedAnims = {}
-
-function Animation.PlayAnimation(char, animationId)
-	if not char then return end
-	
-	local human = char:FindFirstChild("Humanoid")
-	if not human then
-		warn("Humanoid not found in character!")
-		return
-	end
-	
-	local animator = human:FindFirstChildOfClass("Animator") or human:WaitForChild("Animator")
-
-	if not cachedAnims[animationId] then
-		local anim = Instance.new("Animation")
-		anim.AnimationId = "rbxassetid://" .. tostring(animationId)
-		cachedAnims[animationId] = anim
-	end
-
-	local track = animator:LoadAnimation(cachedAnims[animationId])
-	track:Play()
-end
-
-function Animation.PlayAnimationThatHasAnAnimationEvent(char, animationId)
-	if not char then return end
-	
-	local human = char:FindFirstChild("Humanoid")
-	if not human then
-		warn("Humanoid not found in character!")
-		return
-	end
-	
-	local animator = human:FindFirstChildOfClass("Animator") or human:WaitForChild("Animator")
-
-	if not cachedAnims[animationId] then
-		local anim = Instance.new("Animation")
-		anim.AnimationId = "rbxassetid://" .. tostring(animationId)
-		cachedAnims[animationId] = anim
-	end
-
-	local track = animator:LoadAnimation(cachedAnims[animationId])
-	track:Play()
-	return track
-end
-
-function Animation.CreateAnimation(char, animationId)
-	if not char then return end
-
-	local human = char:FindFirstChild("Humanoid")
-	if not human then
-		warn("Humanoid not found in character!")
-		return
-	end
-	
-	if not cachedAnims[animationId] then
-		local anim = Instance.new("Animation")
-		anim.AnimationId = "rbxassetid://" .. tostring(animationId)
-		cachedAnims[animationId] = anim
-	end
-	
-	local animator = human:FindFirstChildOfClass("Animator") or human:WaitForChild("Animator")
-	local track = animator:LoadAnimation(cachedAnims[animationId])
-	
-	return track
-end
-
-return Animation]]
-
 local Animation = {}
 local cachedAnims = {}
 local activeTracks = {} 
@@ -87,6 +17,8 @@ game:GetService("Players").PlayerRemoving:Connect(function(player)
 		cleanupCharacter(player.Character)
 	end
 end)
+
+
 
 function Animation.PlayAnimation(char, animationId)
 	if not char then return end
@@ -137,6 +69,90 @@ end
 
 function Animation.CreateAnimation(char, animationId)
 	return Animation.PlayAnimation(char, animationId)
+end
+
+function Animation.PlayAndHoldAnimation(char, animationId)
+	if not char then return end
+	local human = char:FindFirstChild("Humanoid")
+	if not human then
+		warn("Humanoid not found in character!")
+		return
+	end
+
+	if not activeTracks[char] then
+		activeTracks[char] = {}
+		char.AncestryChanged:Connect(function(_, parent)
+			if not parent then
+				cleanupCharacter(char)
+			end
+		end)
+	end
+
+	if not cachedAnims[animationId] then
+		local anim = Instance.new("Animation")
+		anim.AnimationId = "rbxassetid://" .. tostring(animationId)
+		cachedAnims[animationId] = anim
+	end
+
+	local animator = human:FindFirstChildOfClass("Animator") or human:WaitForChild("Animator")
+	local track = animator:LoadAnimation(cachedAnims[animationId])
+	table.insert(activeTracks[char], track)
+
+	local manualStop = false
+	local conn
+	
+	local originalSpeed = track.Speed 
+	conn = track.Stopped:Connect(function()
+		conn:Disconnect()
+		conn = nil
+
+		if not manualStop then
+			track.TimePosition = track.Length - 0.01
+			track:AdjustSpeed(0)
+		end
+	end)
+
+	track:Play()
+	return track, function()
+		manualStop = true
+
+		if conn then
+			conn:Disconnect()
+			conn = nil
+		end
+		
+		track:AdjustSpeed(originalSpeed)
+
+		if activeTracks[char] then
+			for i, t in ipairs(activeTracks[char]) do
+				if t == track then
+					table.remove(activeTracks[char], i)
+					break
+				end
+			end
+		end
+		track:Stop()
+		track:Destroy()
+	end
+end
+
+
+
+
+function Animation.StopAndCleanup(char, track)
+	if not char or not track then return end
+
+	if activeTracks[char] then
+		for i, t in ipairs(activeTracks[char]) do
+			if t == track then
+				table.remove(activeTracks[char], i)
+				break
+			end
+		end
+	end
+
+	track:Stop()
+	track:Destroy()
 end
 
 function Animation.StopAllAnimations(char)
